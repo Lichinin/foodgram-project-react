628a22aafa966e6cdbe47a5080eaa20bb5e33f06
Виталий Личинин <lichinin.v@yandex.ru>
2023-05-20 14:33:42 +0300

    config flake8 ignores

--- backend/api/views.py
+++ backend/api/views.py
@@ -44,6 +44,7 @@ class CustomUserViewSet(UserViewSet):
             )
             subscription.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
+        return None
 
     @action(
         detail=False,
