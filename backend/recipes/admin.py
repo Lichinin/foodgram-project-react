268759da3d178e6abe6321902893e55326ea7011
Виталий Личинин <lichinin.v@yandex.ru>
2023-05-25 22:00:40 +0300

    django-admin ingredients search field

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -14,7 +14,7 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(Ingredients)
 class IngredientsAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
-    list_filter = ('name',)
+    search_fields = ['name__icontains', ]
 
 
 @admin.register(Recipe)
